eb1625c1cb666b5754a2b8ff516631d3c64c5f74
Fatima Francis <fatimafrancis545@gmail.com>
2018-01-20 14:50:42 -0500

    UI Restyling

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 // get the modal element
-var modal = document.getElementById('portfolio-modal1');
+var modal1 = document.getElementById('portfolio-modal1');
+var modal2 = document.getElementById('portfolio-modal2');
+var modal3 = document.getElementById('portfolio-modal3');
 // get open modal button
-var modalBtn = document.getElementById('portfolio-item1');
-// get close button
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
+var modalBtn1 = document.getElementById('portfolio-item1');
+var modalBtn2 = document.getElementById('portfolio-item2');
+var modalBtn3 = document.getElementById('portfolio-item3');
 
-// listen for open click
-modalBtn.addEventListener('click', openModal);
-
-// listen for close click
-closeBtn.addEventListener('click', closeModal);
+document.getElementById('portfolio-item1').onclick = function() {
+	modal1.style.display = 'block';
+}
+document.getElementById('portfolio-item2').onclick = function() {
+	modal2.style.display = 'block';
+}
+document.getElementById('portfolio-item3').onclick = function() {
+	modal3.style.display = 'block';
+}
+document.getElementsByClassName('closeBtn1')[0].onclick = function() {
+	modal1.style.display = 'none';
+}
+document.getElementsByClassName('closeBtn2')[0].onclick = function() {
+	modal2.style.display = 'none';
+}
+document.getElementsByClassName('closeBtn3')[0].onclick = function() {
+	modal3.style.display = 'none';
+}
 
 // listen for outside click
 window.addEventListener('click', outsideClick);
-
-// function to open modal
-function openModal() {
-	modal.style.display = 'block';
-	
-}
-
-// function to close modal
-function closeModal() {
-	modal.style.display = 'none';
-	
-}
 
 // function to close modal if outside click
 function outsideClick(e) {
